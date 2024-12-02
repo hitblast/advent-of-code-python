833331915aff464d8665dcf7ddf9447d51f5623c
HitBlast <hitblastlive@gmail.com>
2024-12-03 00:50:44 +0600

    basic tests for current solutions

---
+++ tests/test_2024.py
@@ -0,0 +1,21 @@
+# Imports.
+import subprocess
+
+# Test parts and solutions key:value store.
+test_data = {
+    "1:1": "1603498",
+    "1:2": "25574739",
+    "2:1": "306",
+    "2:2": "366",
+}
+
+
+# Test the Advent of Code 2024 solutions.
+def test_2024() -> None:
+    for part, expected_output in test_data.items():
+        result = subprocess.run(
+            f"advent run {part} --year 2024 --use-preincluded-data",
+            shell=True,
+            capture_output=True,
+        ).stdout.decode("utf-8")
+        assert expected_output in result
