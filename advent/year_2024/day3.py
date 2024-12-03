3879ff2e12963ab67a58fb5f68c773fafa30fa62
HitBlast <hitblastlive@gmail.com>
2024-12-03 11:18:22 +0600

    documented code

--- advent/year_2024/day3.py
+++ advent/year_2024/day3.py
@@ -6,9 +6,11 @@ import re
 def part1(data: str) -> int:
     result = 0
 
+    # Extract the mul functions [mul(a, b)] and their arguments.
     mul_functions = re.findall(r"mul\(\d+,\d+\)", data)
     arguments = [re.findall(r"\d+", mul_function) for mul_function in mul_functions]
 
+    # Multiply the arguments and add them to the result.
     for arg1, arg2 in arguments:
         result += int(arg1) * int(arg2)
 
@@ -20,13 +22,14 @@ def part2(data: str) -> int:
     result = 0
     enabled = True
 
+    # Extract the instructions [mul(a, b), do(), don't()]
     instructions = re.findall(r"(mul\(\d+,\d+\)|do\(\)|don't\(\))", data)
 
     for instruction in instructions:
         if instruction == "do()":
             enabled = True
         elif instruction == "don't()":
-            enabled = False
+            enabled = False  # Disable the multiplication right here!
         else:
             if enabled:
                 arg1, arg2 = re.findall(r"\d+", instruction)
